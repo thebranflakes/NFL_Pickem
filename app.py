cf29c5e27412f69eb18be583073a2a26e346d50d
thebranflakes <hernanb2@oregonstate.edu>
2022-10-11 18:18:46 -0400

    Redirect logout back to login page, and log out button on 'My Picks' page.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "secretKEYsoSECRET"
+app.permanent_session_lifetime = timedelta(days=5)
 
 
 @app.route("/")
@@ -12,18 +14,27 @@ def home():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
+        session.permanent = True
         username = request.form["nm"]
         session["username"] = username
         return redirect(url_for("username"))
     else:
+        if "username" in session:
+            return redirect(url_for("username"))
         return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("login"))
 
 
 @app.route("/username")
 def username():
     if "username" in session:
         username = session["username"]
-        return f"<h1>{username}</h1>"
+        return render_template("user.html")
     else:
         return redirect(url_for("login"))
 
