49ab638c04e729f46afbd638c7b4473fc53e981f
thebranflakes <hernanb2@oregonstate.edu>
2022-12-14 02:38:45 -0500

    changed user page back to /user instead of /username as it wouldn't change with that users username. Added flash messages for sign in and sign out.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 
 app = Flask(__name__)
@@ -26,16 +26,20 @@ def login():
 
 @app.route("/logout")
 def logout():
+    if "username" in session:
+        username = session["username"]
+        flash(f"You have been logged out, {username}", "info")
     session.pop("username", None)
     return redirect(url_for("login"))
 
 
-@app.route("/username")
+@app.route("/user")
 def username():
     if "username" in session:
         username = session["username"]
-        return render_template("user.html")
+        return render_template("user.html", username=username)
     else:
+        flash("You are not logged in!")
         return redirect(url_for("login"))
 
 
